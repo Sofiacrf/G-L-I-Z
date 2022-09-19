6d8233b4ab12ae81a8dfbbbbc3bdc682704c16ac
sofiacrf <sofia.rojasflo@gmail.com>
2022-09-19 17:21:52 -0500

    finished footer

--- lawyers/src/components/Footer/FooterComponent.js
+++ lawyers/src/components/Footer/FooterComponent.js
@@ -1,9 +1,91 @@
-import React from 'react'
+import React from "react";
+import bootstrapLogo from "../../assets/bootstrap-img.png";
+import "./footer.css"
 
 function FooterComponent() {
   return (
-    <div>FooterComponent</div>
-  )
+    <section className="footer bd-footer bg-light">
+      <div className="container py-5">
+        <div className="row">
+          <div className="col-lg-3 mb-3">
+            <a
+              className="d-inline-flex align-items-center mb-2 link-dark text-decoration-none"
+              href="/"
+              aria-label="Lawyers"
+            >
+              <img
+                src={bootstrapLogo}
+                width="40"
+                height="32"
+                className="d-block me-2"
+              />
+            </a>
+            <h5>Lawyers</h5>
+            <ul className="list-unstyled small text-muted">
+              <li className="mb-2">
+                <a>
+                  {" "}
+                  Lorem ipsum sit amet, consectetur adipiscing elit. Proin
+                  vestibulum non sapien nec cursus.
+                </a>
+              </li>
+            </ul>
+          </div>
+
+          {/* Second column */}
+          <div className="col-6 col-lg-2 offset-lg-1 mb-3">
+            <h5>Links</h5>
+            <ul className="list-unstyled">
+              <li className="mb-2">
+                <a href="/">Home</a>
+              </li>
+              <li className="mb-2">
+                <a href="/quienes-somos">Quiénes somos</a>
+              </li>
+              <li className="mb-2">
+                <a href="/nuestros-servicios">Nuestros servicios</a>
+              </li>
+              <li className="mb-2">
+                <a href="/contact">Contacto</a>
+              </li>
+            </ul>
+          </div>
+
+          {/* Third Column */}
+          <div className="col-6 col-lg-2 offset-lg-1 mb-3">
+            <h5>Ubicación</h5>
+            <ul className="list-unstyled text-muted">
+              <li className="mb-2">
+                <a>Av. México, Ciudad de México, 01582</a>
+              </li>
+              <li className="mb-2">
+                <a>55.12.34.56.78.9</a>
+              </li>
+              <li className="mb-2">
+                <a>email@me.com</a>
+              </li>
+            </ul>
+          </div>
+
+          {/* Fourth Column */}
+          <div className="col-lg-3 mb-3">
+            <a
+              className="d-inline-flex align-items-center mb-2 link-dark text-decoration-none"
+              href="/"
+              aria-label="Lawyers"
+            ></a>
+            <ul className="list-unstyled small text-muted">
+              <li className="mb-2">
+                Designed and built by the company "
+                <a href="https://rojastechnologies.com/">Rojas Technologies</a>
+                ".
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
 
-export default FooterComponent
+export default FooterComponent;
--- lawyers/src/components/NavBar/NavbarComponent.js
+++ lawyers/src/components/NavBar/NavbarComponent.js
@@ -1,25 +1,32 @@
-import React from 'react'
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
+import React from "react";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import bootstrapLogo from "../../assets/bootstrap-img.png";
 
 function NavbarComponent() {
   return (
     <Navbar bg="light" expand="lg">
-    <Container>
-      <Navbar.Brand href="#home">Lawyers</Navbar.Brand>
-      <Navbar.Toggle aria-controls="basic-navbar-nav" />
-      <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className="me-auto">
-          <Nav.Link href="#">Home</Nav.Link>
-          <Nav.Link href="/quienes-somos">Quiénes Somos</Nav.Link>
-          <Nav.Link href="/nuestros-servicios">Nuestros Servicios</Nav.Link>
-          <Nav.Link href="/contacto">Contacto</Nav.Link>
-        </Nav>
-      </Navbar.Collapse>
-    </Container>
-  </Navbar>
-  )
+      <Container>
+        <img
+          src={bootstrapLogo}
+          width="40"
+          height="32"
+          className="d-block me-2"
+        />
+        <Navbar.Brand href="#home">Lawyers</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#">Home</Nav.Link>
+            <Nav.Link href="/quienes-somos">Quiénes Somos</Nav.Link>
+            <Nav.Link href="/nuestros-servicios">Nuestros Servicios</Nav.Link>
+            <Nav.Link href="/contacto">Contacto</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
 }
 
-export default NavbarComponent
+export default NavbarComponent;
